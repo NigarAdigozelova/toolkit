0f780be68aefcccb3b2425e7a4c1b6ae3a17b0a1
NigarAdigozelova <72kwbg1@code.edu.az>
2023-01-12 12:01:30 +0400

    modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Counter } from './features/Counter';
+import Input from './features/Input';
+import Todos from './features/Todos';
 
 function App() {
   return (
     <div >
-      <Counter/>
+      <Counter/><br/>
+      <Input/>
+      <Todos/>
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "../features/counterSlice";
+import todoSlice from "../features/todoSlice";
 export const store = configureStore({
     reducer: {
         counter: counterSlice,
+        todo:todoSlice
     },
 })
